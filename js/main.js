4350d505f3f46d6feca5cc70e14139bfe8da6894
Russell Kennedy <russell.j.kennedy@googlemail.com>
2019-02-26 16:08:12 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,122 @@
+// Main site JS
+
+// Document ready functions
+jQuery(document).ready(function($) {
+
+  $.get('assets/george.txt', function(data){
+    // Grab all quotes, separated by line breaks
+    var quotes = data.split("\n");
+
+    $currentCount = 1;
+
+    // Add a quote
+    $('#george').click(function() {
+      var quote = Math.floor(quotes.length * Math.random());
+
+        $('.add').append(quotes[quote]);
+        $('.add').append("&nbsp;");
+
+        $currentQuote = (quotes[quote]);
+        console.log($currentQuote);
+        // $currentCount++;
+        // console.log($currentCount);
+
+    });
+
+    $('#costanza').click(function() {
+      console.log($currentQuote);
+      $($currentQuote).remove();
+  });
+
+
+
+});
+
+
+  // $findQuote = (function() {
+    //     $.ajax({
+      //         url : "assets/george.txt",
+      //         dataType: "text",
+      //         success : function (data) {
+        //             // $(".add").html(data.split('\n'));
+        //             $quote = shuffle(data.split('\n'));
+        //         }
+        //     });
+        // });
+
+        //   var gc = new Array;
+        //
+        //   $.get('assets/george.txt', function(data) {
+          //    // do_something_with(data)
+          //    gc = data.split("\n");
+          //    // $(".add").html(gc);
+          //    // console.log(gc);
+          // });
+
+  // $count = 1;
+  // $quote = '';
+  // $findQuote = function() {
+  //   $.get('assets/george.txt').success(function(data){
+  //     $quote = random(data.split('\n'));
+  //   });
+  // };
+
+  // $findQuote = (function() {
+  //     $.ajax({
+  //         url : "assets/george.txt",
+  //         dataType: "text",
+  //         success : function (data) {
+  //             // $(".add").html(data.split('\n'));
+  //             $quote = random(data.split('\n'));
+  //             console.log($quote);
+  //           }
+  //     });
+  // });
+  //
+  // var random = function(data) {
+  //   for (var j, x, i = data.length; i; j = parseInt(Math.random() * i, 10), x = data[--i], data[i] = data[j], data[j] = x);
+  //   return data;
+  // };
+
+
+
+// /* Controllers */
+// myApp.controller('IpsumCtrl', ['$scope', '$http', function($scope, $http) {
+//   $scope.quoteCount = 1;
+//   $scope.ipsum = '';
+//   $scope.getIpsum = function() {
+//     $http.get('/ipsum/words.txt').success(function(data) {
+//       $scope.ipsum = shuffle(data.split('\n'));
+//     });
+//   };
+//   var shuffle = function(data) {
+//     for (var j, x, i = data.length; i; j = parseInt(Math.random() * i, 10), x = data[--i], data[i] = data[j], data[j] = x);
+//     return data;
+//   };
+//   $scope.tooFew = function() {
+//     if ($scope.quoteCount < 2) return true;
+//     else return false;
+//   };
+//   $scope.getIpsum();
+//   $scope.addIpsum = function() {
+//     $scope.quoteCount = $scope.quoteCount + 1;
+//   };
+//   $scope.lessIpsum = function() {
+//     $scope.quoteCount = $scope.quoteCount - 1;
+//   };
+// }]);
+//
+// function selectText(containerid) {
+//   if (document.selection) {
+//     var range = document.body.createTextRange();
+//     range.moveToElementText(document.getElementById(containerid));
+//     range.select();
+//   } else if (window.getSelection) {
+//     var range = document.createRange();
+//     range.selectNode(document.getElementById(containerid));
+//     window.getSelection().addRange(range);
+//   }
+// }
+
+
+});
