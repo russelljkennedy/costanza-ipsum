76933ff5a348d176121e383fa5472f80616e49b0
Russell Kennedy <russell.j.kennedy@googlemail.com>
2019-02-28 09:28:04 +0000

    jQuery for new paragraphs tweaked

--- js/main.js
+++ js/main.js
@@ -2,14 +2,13 @@
 
 // Document ready functions
 jQuery(document).ready(function($) {
+
   // Add a new paragraph
   function newPara() {
     $quoteBlock = $('#quoteblock');
-    $blockCount = 1;
     // Change the previous quoteblock id and append a new quoteblock - this way new quotes will always be added to the #quoteblock element. Increment the block count
-    $quoteBlock.attr("id", "quoteblock_" + $blockCount);
+    $quoteBlock.attr('id', 'quoteblock_1');
     $('.add').append("<div id='quoteblock'>");
-    $blockCount++;
   };
     // Grab the quote file from the server
     $.get('assets/george.txt', function(data){
@@ -21,28 +20,40 @@ jQuery(document).ready(function($) {
         // Increment the quote count, pick one at random to add, assign it an ID so it can be removed. Display it with a space afterwards
         $totalQuotes++;
         $chosenQuote = Math.floor(quotes.length * Math.random());
-        $quoteId = "quote_" + $totalQuotes;
-        $displayQuote = (quotes[$chosenQuote] + " ");
+        $quoteId = 'quote_' + $totalQuotes;
+        $displayQuote = (quotes[$chosenQuote] + ' ');
         // Add a div for the quotes to sit in. Used later by the newPara function
         if($totalQuotes == 1) {
-            $('.add').append("<div id='quoteblock'><span id='" + $quoteId + "'>" + $displayQuote + "</span>");
+            $('.add').append('<div id="quoteblock"><span id="' + $quoteId + '">' + $displayQuote + '</span>');
         }
         else {
-            $('#quoteblock').append("<span id='" + $quoteId + "'>" + $displayQuote + "</span>");
+            $('#quoteblock').append('<span id="' + $quoteId + '">' + $displayQuote + '</span>');
         }
       });
       // Remove a quote from the DOM using the ID set above. Decrement the quote count accordingly
       $('#costanza').click(function() {
-        $("#quote_" + $totalQuotes).remove();
+        $('#quote_' + $totalQuotes).remove();
+        // Check to see if the quoteblock is empty. If it is, remove it and set the last child as the active block
+        $quoteBlock = $('#quoteblock');
+        $lastQuoteBlock = $('.add #quoteblock_1:last');
+        if($quoteBlock.text() == '') {
+          $quoteBlock.remove();
+          $lastQuoteBlock.attr('id', 'quoteblock');
+        }
         $totalQuotes--;
       });
       // Add a new paragraph on button click
       $('#art').click(function() {
         newPara();
+        $totalQuotes++;
+        $chosenQuote = Math.floor(quotes.length * Math.random());
+        $quoteId = 'quote_' + $totalQuotes;
+        $displayQuote = (quotes[$chosenQuote] + ' ');
+        $('#quoteblock').append("<span id='" + $quoteId + "'>" + $displayQuote + "</span>");
       });
       // Clear all the ipsum and reset the quote count
-      $('#vanderlay').click(function() {
-        $("[id^=quoteblock]").remove();
+      $('#vandelay').click(function() {
+        $('[id^=quoteblock]').remove();
         $totalQuotes = 0;
       });
     });
