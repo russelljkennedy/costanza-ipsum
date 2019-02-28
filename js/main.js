1e2f8d1c7e5edc904ec908812467025bf72952f5
Russ Kennedy <russell.j.kennedy@googlemail.com>
2019-02-28 20:07:37 +0000

    Added inactive class to less button when there are no quotes on the page

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@
 jQuery(document).ready(function($) {
   // Add a new paragraph
   function newPara() {
-    $quoteBlock = $('#quoteblock');
-    // Change the previous quoteblock id and append a new quoteblock - this way new quotes will always be added to the #quoteblock element. Increment the block count
-    $quoteBlock.attr('id', 'quoteblock_1');
-    $('.add').append("<div id='quoteblock'>");
+    $yadaBlock = $('#yadablock');
+    // Change the previous yadablock id and append a new yadablock - this way new quotes will always be added to the #yadablock element. Increment the block count
+    $yadaBlock.attr('id', 'yadablock_1');
+    $('.yada-yada-area').append("<div id='yadablock'>");
   };
     // Grab the quote file from the server
     $.get('assets/george.txt', function(data){
@@ -23,25 +23,29 @@ jQuery(document).ready(function($) {
         $displayQuote = (quotes[$chosenQuote] + ' ');
         // Add a div for the quotes to sit in. Used later by the newPara function
         if($totalQuotes == 1) {
-            $('.add').append('<div id="quoteblock"><span id="' + $quoteId + '">' + $displayQuote + '</span>');
+            $('.yada-yada-area').append('<div id="yadablock"><span id="' + $quoteId + '">' + $displayQuote + '</span>');
+            $('#costanza').removeClass('unemployed');
         }
         else {
-            $('#quoteblock').append('<span id="' + $quoteId + '">' + $displayQuote + '</span>');
+            $('#yadablock').append('<span id="' + $quoteId + '">' + $displayQuote + '</span>');
         }
       });
       // Remove a quote from the DOM using the ID set above
       $('#costanza').click(function() {
         $('#quote_' + $totalQuotes).remove();
-        // Check to see if the quoteblock is empty. If it is, remove it and set the last child as the active block
-        $quoteBlock = $('#quoteblock');
-        $lastQuoteBlock = $('.add #quoteblock_1:last');
-        if($quoteBlock.text() == '') {
-          $quoteBlock.remove();
-          $lastQuoteBlock.attr('id', 'quoteblock');
+        // Check to see if the yadablock is empty. If it is, remove it and set the last child as the active block
+        $yadaBlock = $('#yadablock');
+        $lastYadaBlock = $('.yada-yada-area #yadablock_1:last');
+        if($yadaBlock.text() == '') {
+          $yadaBlock.remove();
+          $lastYadaBlock.attr('id', 'yadablock');
         }
         // Only decrement the quote count if there are quotes on the page
         if ($totalQuotes > 0) {
             $totalQuotes--;
+        };
+        if ($totalQuotes == 0) {
+          $('#costanza').addClass('unemployed');
         }
       });
       // Add a new paragraph on button click
@@ -51,12 +55,16 @@ jQuery(document).ready(function($) {
         $chosenQuote = Math.floor(quotes.length * Math.random());
         $quoteId = 'quote_' + $totalQuotes;
         $displayQuote = (quotes[$chosenQuote] + ' ');
-        $('#quoteblock').append("<span id='" + $quoteId + "'>" + $displayQuote + "</span>");
+        $('#yadablock').append("<span id='" + $quoteId + "'>" + $displayQuote + "</span>");
+        if($totalQuotes == 1) {
+          $('#costanza').removeClass('unemployed');
+        }
       });
       // Clear all the ipsum and reset the quote count
       $('#vandelay').click(function() {
-        $('[id^=quoteblock]').remove();
+        $('[id^=yadablock]').remove();
         $totalQuotes = 0;
+        $('#costanza').addClass('unemployed');
       });
     });
 });
