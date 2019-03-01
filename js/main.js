4e2739575f10554c4a51e56d33ee9970ed45caa3
Russ Kennedy <russell.j.kennedy@googlemail.com>
2019-03-01 16:41:54 +0000

    Some responsive styling + more quotes added

--- js/main.js
+++ js/main.js
@@ -1,54 +1,68 @@
 // Main site JS
-
-// Document ready functions
 jQuery(document).ready(function($) {
-  // Add a new paragraph
+
+  // Add a new paragraph.
   function newPara() {
     $yadaBlock = $('#yadablock');
-    // Change the previous yadablock id and append a new yadablock - this way new quotes will always be added to the #yadablock element. Increment the block count
+
+    // Change the previous block id and append a new one - this way new quotes will always be added to the #yadablock element.
     $yadaBlock.attr('id', 'yadablock_1');
     $('.yada-yada-area').append("<div id='yadablock'>");
   };
-    // Grab the quote file from the server
+
+    // Grab the quote file from the server.
     $.get('assets/george.txt', function(data){
-      // Set a var for the number of quotes added, then fetch everything from the txt file, split by line breaks
+
+      // Set a var for the number of quotes added, then fetch everything from the txt file, split by line breaks.
       $totalQuotes = 0;
       var quotes = data.split("\n");
-      // Add a quote to the DOM
+
+      // Add a quote to the DOM.
       $('#george').click(function() {
-        // Increment the quote count, pick one at random to add, assign it an ID so it can be removed. Display it with a space afterwards
+
+        // Increment the quote count, pick one at random to add, and assign it an ID so it can be removed later on. Display it with a space afterwards for better formatting.
         $totalQuotes++;
         $chosenQuote = Math.floor(quotes.length * Math.random());
         $quoteId = 'quote_' + $totalQuotes;
         $displayQuote = (quotes[$chosenQuote] + ' ');
-        // Add a div for the quotes to sit in. Used later by the newPara function
+
+        // Add a div for the quotes to sit in. Used later by the newPara function.
         if($totalQuotes == 1) {
-            $('.yada-yada-area').append('<div id="yadablock"><span id="' + $quoteId + '">' + $displayQuote + '</span>');
-            $('#costanza').removeClass('unemployed');
+          $('.yada-yada-area').append('<div id="yadablock"><span id="' + $quoteId + '">' + $displayQuote + '</span>');
+          // Make the remove and clear buttons active.
+          $('#costanza').removeClass('unemployed');
+          $('#vandelay').removeClass('unemployed');
         }
         else {
-            $('#yadablock').append('<span id="' + $quoteId + '">' + $displayQuote + '</span>');
+          $('#yadablock').append('<span id="' + $quoteId + '">' + $displayQuote + '</span>');
         }
       });
-      // Remove a quote from the DOM using the ID set above
+
+      // Remove a quote from the DOM using the ID set above.
       $('#costanza').click(function() {
         $('#quote_' + $totalQuotes).remove();
-        // Check to see if the yadablock is empty. If it is, remove it and set the last child as the active block
+
+        // Check to see if the yadablock is empty. If it is, remove it and set the last child as the active block.
         $yadaBlock = $('#yadablock');
         $lastYadaBlock = $('.yada-yada-area #yadablock_1:last');
         if($yadaBlock.text() == '') {
           $yadaBlock.remove();
           $lastYadaBlock.attr('id', 'yadablock');
         }
+
         // Only decrement the quote count if there are quotes on the page
         if ($totalQuotes > 0) {
             $totalQuotes--;
         };
+
+        // If there are no quotes on the page, make the remove and clear buttons inactive again.
         if ($totalQuotes == 0) {
           $('#costanza').addClass('unemployed');
+          $('#vandelay').addClass('unemployed');
         }
       });
-      // Add a new paragraph on button click
+
+      // Add a new paragraph on button click.
       $('#art').click(function() {
         newPara();
         $totalQuotes++;
@@ -56,15 +70,21 @@ jQuery(document).ready(function($) {
         $quoteId = 'quote_' + $totalQuotes;
         $displayQuote = (quotes[$chosenQuote] + ' ');
         $('#yadablock').append("<span id='" + $quoteId + "'>" + $displayQuote + "</span>");
+
+        // If the new paragraph is clicked before the add function, make the remove and clear buttons active.
         if($totalQuotes == 1) {
           $('#costanza').removeClass('unemployed');
+          $('#vandelay').removeClass('unemployed');
         }
       });
-      // Clear all the ipsum and reset the quote count
+
+      // Clear all the ipsum and reset the quote count. Make the remove and clear buttons inactive.
       $('#vandelay').click(function() {
         $('[id^=yadablock]').remove();
         $totalQuotes = 0;
         $('#costanza').addClass('unemployed');
+        $('#vandelay').addClass('unemployed');
       });
     });
+
 });
